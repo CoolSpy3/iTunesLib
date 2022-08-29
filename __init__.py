b17815015c957c6081dd7d0ff908df4c18f8869c
CoolSpy3 <coolspythree@gmail.com>
2022-08-28 22:07:53 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,56 @@
+import json
+import xml.etree.ElementTree
+
+
+def addArray(a):
+    parsedDict = "["
+
+    for child in a:
+        if child.tag == "array":
+            parsedDict += addArray(child)
+        elif child.tag == "dict":
+            parsedDict += addDict(child)
+        elif child.tag == "string" or child.tag == "date" or child.tag == "data":
+            parsedDict += "\"" + str(child.text).replace('"', '\\"').replace('\n', '').replace('\r', '').replace('\t', '') + "\","
+        elif child.tag == "true":
+            parsedDict += "true,"
+        elif child.tag == "false":
+            parsedDict += "false,"
+        else:
+            parsedDict += child.text
+            parsedDict += ","
+
+    return parsedDict[:-1] + "],"
+
+def addDict(d):
+    parsedDict = "{"
+
+    for child in d:
+        if child.tag == "key":
+            parsedDict += "\"" + child.text + "\": "
+        elif child.tag == "array":
+            parsedDict += addArray(child)
+        elif child.tag == "dict":
+            parsedDict += addDict(child)
+        elif child.tag == "string" or child.tag == "date" or child.tag == "data":
+            parsedDict += "\"" + child.text.replace('"', '\\"').replace('\n', '').replace('\r', '').replace('\t', '') + "\","
+        elif child.tag == "true":
+            parsedDict += "true,"
+        elif child.tag == "false":
+            parsedDict += "false,"
+        else:
+            parsedDict += child.text
+            parsedDict += ","
+
+    return parsedDict[:-1] + "},"
+
+def readXML(file):
+    tree = xml.etree.ElementTree.parse(file)
+    root = tree.getroot()
+    parsedFile = ""
+    for child in root:
+        if child.tag == "dict":
+            parsedFile += addDict(child)
+    with open('tmp.json', 'wb') as file:
+        file.write(parsedFile.encode('utf-8', 'ignore'))
+    return json.loads(parsedFile[:-1])
